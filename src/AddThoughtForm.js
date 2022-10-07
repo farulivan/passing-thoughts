236d43bb5ab22932fe04a3b1bc6d05c829854b4f
Farul Ivan <farulivan@gmail.com>
2022-10-07 10:06:06 +0700

    finish the project

---
+++ src/AddThoughtForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { generateId, getNewExpirationTime } from './utilities';
+
+export function AddThoughtForm(props) {
+  const [text, setText] = useState('')
+
+  const handleTextChange = ({target}) => {
+    setText(target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const thought = {
+      id: generateId(),
+      text: text,
+      expiresAt: getNewExpirationTime()
+    }
+    text && props.addThought(thought)
+    setText('')
+  }
+
+  return (
+    <form className="AddThoughtForm" onSubmit={handleSubmit} >
+      <input
+        type="text"
+        aria-label="What's on your mind?"
+        placeholder="What's on your mind?"
+        value={text}
+        onChange={handleTextChange}
+      />
+      <input type="submit" value="Add" />
+    </form>
+  );
+}
